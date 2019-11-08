40029354e91615424667bf8af0852873bbc489b6
jean-smaug <maximeblanc.dev@gmail.com>
2019-11-08 12:21:50 +0100

    feat: add smaug image

--- pages/custom-about.js
+++ pages/custom-about.js
@@ -3,6 +3,7 @@ import React from 'react'
 const About = () => (
   <div>
     <div className='hero'>
+        <img src="/images/smaug.jpg" width="100" height="100" />
         sqds
     </div>
   </div>
